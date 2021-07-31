d17f6c6fd3ac84ddda4cc8e37d2f719259d0fbc2
Lukas Siemon <github@blackflux.com>
2021-07-31 16:47:50 -0700

    chore: coverage

--- test/index/files.spec.js
+++ test/index/files.spec.js
@@ -5,6 +5,7 @@ const Slack = require('../../src/index');
 
 describe('Testing Slack SDK', {
   useNock: true,
+  nockStripHeaders: true,
   cryptoSeed: '3eced0c2-38ea-4bc4-9045-ee0cb68bd688',
   timestamp: '2020-07-13T16:14:25.488Z'
 }, () => {
@@ -17,5 +18,11 @@ describe('Testing Slack SDK', {
     const filepath = path.join(`${__filename}__fixtures`, 'file.jpg');
     const r = await slack.files.upload(filepath);
     expect(r).to.deep.equal({ id: 'F029QHN0AAZ', title: 'file.jpg' });
+  });
+
+  it('Testing files.upload with custom title and filename', async () => {
+    const filepath = path.join(`${__filename}__fixtures`, 'file.jpg');
+    const r = await slack.files.upload(filepath, '<some-title>', '<some-filename>');
+    expect(r).to.deep.equal({ id: 'F02AH8J752L', title: '<some-title>' });
   });
 });
