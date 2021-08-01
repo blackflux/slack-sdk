a134e66d844abe828af3b47e6baa8f7b5a70a0ef
Lukas Siemon <github@blackflux.com>
2021-07-31 17:00:02 -0700

    chore: refactor

--- src/index/channel.js
+++ src/index/channel.js
@@ -1,34 +1,26 @@
-module.exports = (call) => {
-  const channelMeta = async (name) => {
-    const rtmStart = await call('rtm.start', {}, true);
-    const channel = rtmStart.channels.find((chan) => chan.name === name);
-    if (!channel) {
-      throw new Error(`Channel "${name}" not found.`);
-    }
-    return channel;
-  };
-  return {
-    meta: channelMeta,
-    message: async (name, msg) => {
-      const channel = await channelMeta(name);
-      return call('chat.postMessage', {
-        text: msg,
-        channel: channel.id
-      });
-    },
-    setTopic: async (name, topic) => {
-      const channel = await channelMeta(name);
-      return call('conversations.setTopic', {
-        topic,
-        channel: channel.id
-      });
-    },
-    setPurpose: async (name, purpose) => {
-      const channel = await channelMeta(name);
-      return call('conversations.setPurpose', {
-        purpose,
-        channel: channel.id
-      });
-    }
-  };
-};
+const getChannelMeta = require('./util/get-channel-meta');
+
+module.exports = (call) => ({
+  meta: (name) => getChannelMeta(call, name),
+  message: async (name, msg) => {
+    const channel = await getChannelMeta(call, name);
+    return call('chat.postMessage', {
+      text: msg,
+      channel: channel.id
+    });
+  },
+  setTopic: async (name, topic) => {
+    const channel = await getChannelMeta(call, name);
+    return call('conversations.setTopic', {
+      topic,
+      channel: channel.id
+    });
+  },
+  setPurpose: async (name, purpose) => {
+    const channel = await getChannelMeta(call, name);
+    return call('conversations.setPurpose', {
+      purpose,
+      channel: channel.id
+    });
+  }
+});
---
+++ src/index/util/get-channel-meta.js
@@ -0,0 +1,8 @@
+module.exports = async (call, name) => {
+  const rtmStart = await call('rtm.start', {}, true);
+  const channel = rtmStart.channels.find((chan) => chan.name === name);
+  if (!channel) {
+    throw new Error(`Channel "${name}" not found.`);
+  }
+  return channel;
+};
